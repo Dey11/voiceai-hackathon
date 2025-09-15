1cf888b17f645f4cef4c6a0bda063015fe338168
Dey11 <shreyandey11@gmail.com>
2025-09-16 00:39:43 +0530

    remove prisma accelerate

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,4 +1,2 @@
 import { PrismaClient } from "@prisma/client/edge";
-import { withAccelerate } from "@prisma/extension-accelerate";
-
-export const prisma = new PrismaClient().$extends(withAccelerate());
+export const prisma = new PrismaClient()
